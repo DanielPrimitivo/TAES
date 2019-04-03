d2685093bf09c65b7ea21165d9a0ed95b5294c51
Daniel Primitivo Cano <dpc32@alu.ua.es>
2019-04-03 20:43:37 +0200

    Estructura seeders

---
+++ database/seeds/CoordinatesHTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CoordinatesHTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/CoordinatesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CoordinatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
