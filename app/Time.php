82be5eee7c681758db3b028d1f6f67847f3831c5
Fran García <frandbc7@gmail.com>
2019-04-07 12:30:21 +0200

    Añadidas funciones Time

--- app/Time.php
+++ app/Time.php
@@ -6,8 +6,134 @@ use Illuminate\Database\Eloquent\Model;
 
 class Time extends Model
 {
+    protected $fillable = array('viento', 'fecha', 'dirviento', 'humedad', 'temperatura', 'lluvia', 'prevision');
+    
     public function notice() {
         //Time tiene la clave ajena de Notice
         return $this->belongsTo('App\Notice');
+    }
+
+    public function obtenerInformacion(int $id){
+        $t = Time::where('id', '=', $id);
+        return view('time.time', compact('t'));
+    }
+
+    public function getFecha(int $id){
+        $re = Time::where('id', '=', $id)->fecha;
+    }
+
+    public function setFecha(int $id, String $fecha){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $fecha;
+        $re->viento = $t['viento'];
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getViento(int $id){
+        $re = Time::where('id', '=', $id)->viento;
+    }
+
+    public function setViento(int $id, String $viento){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $viento;
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getHumedad(int $id){
+        $re = Time::where('id', '=', $id)->humedad;
+    }
+
+    public function setHumedad(int $id, String $humedad){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $t['viento'];
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $humedad;
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getTemperatura(int $id){
+        $re = Time::where('id', '=', $id)->temperatura;
+    }
+
+    public function setTemperatura(int $id, String $temperatura){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $t['viento'];
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $temperatura;
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getLluvia(int $id){
+        $re = Time::where('id', '=', $id)->lluvia;
+    }
+
+    public function setLluvia(int $id, String $lluvia){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $t['viento'];
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $lluvia;
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getDirViento(int $id){
+        $re = Time::where('id', '=', $id)->dirviento;
+    }
+
+    public function setDirViento(int $id, String $dir){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $t['viento'];
+        $re->dirviento = $dir;
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $t['prevision'];
+        $t->update($re);
+    }
+
+    public function getPrevision(int $id){
+        $re = Time::where('id', '=', $id)->prevision;
+    }
+
+    public function setPrevision(int $id, String $prev){
+        $t = Time::where('id', '=', $id);
+        $re = new Time();
+        $re->fecha = $t['fecha'];
+        $re->viento = $t['viento'];
+        $re->dirviento = $t['dirviento'];
+        $re->humedad = $t['humedad'];
+        $re->temperatura = $t['temperatura'];
+        $re->lluvia = $t['lluvia'];
+        $re->prevision = $prev;
+        $t->update($re);
     }
 }
