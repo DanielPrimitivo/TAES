f13641270edb04ce9b6b63bc42026cc858ce792e
Daniel Primitivo Cano <dpc32@alu.ua.es>
2019-04-09 13:26:19 +0200

    Lanzador NoticeSeeder

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -11,6 +11,14 @@ class ImagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $info = parse_ini_file("imagen.ini", true);
+        foreach($info as $noti){
+            $notice = new Notice([
+                'fecha' => $noti['fecha'],
+                'valoracion' => $noti['valoracion'],
+                'visto' => $noti['visto']
+            ]);
+            $notice->save();
+        }
     }
 }
--- database/seeds/NoticeTableSeeder.php
+++ database/seeds/NoticeTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Notice;
 
 class NoticeTableSeeder extends Seeder
 {
