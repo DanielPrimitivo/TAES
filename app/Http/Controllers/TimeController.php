64307d5326d136e6a087e8f83e0c90131ee47c30
Javier Agustín Tyszko González <javitg96@gmail.com>
2019-05-01 19:23:58 +0200

    Prototipo getWeather mejorado

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Time;
+use Carbon\Carbon;
 
 
 class TimeController extends Controller
@@ -12,22 +13,52 @@ class TimeController extends Controller
         //llamar a la API para obtener el tiempo
         $lowm = new LaravelOWM();
         if($hours == 0) {
-            $weather = $lowm->getCurrentWeather(array('lat' => $lat, 'lon' => $lon), 'metric', 'es');
+            $weather = $lowm->getCurrentWeather(array('lat' => $lat, 'lon' => $lon), 'es', 'metric');
+            $array = array(
+                'vientoVel' => $weather->wind->speed,
+                'vientoDir' => $weather->wind->direction,
+                'humedad' => $weather->humidity,
+                'temperatura' => $weather->temperature,
+                'temperaturaMin' => $weather->temperature->min,
+                'temperaturaMax' => $weather->temperature->max,
+                'precipitaciones' => $weather->precipitation,
+                'presion' => $weather->pressure,
+                'fecha' => $weather->lastUpdate
+            );
         } else {
-            $weather = $lowm->getWeatherForecast(array('lat' => $lat, 'lon' => $lon), 'metric', 'es', '', 1);
+            $forecast = $lowm->getWeatherForecast(array('lat' => $lat, 'lon' => $lon), 'es', 'metric', 1);
+            $cto = new Carbon($forecast->forecasts[0]->time->to);
+            if(Carbon::now()->greaterThan($cto)) {
+                switch ($hours) {
+                    case 3: $weather = $forecast->forecasts[2];
+                            break;
+                    case 6: $weather = $forecast->forecasts[3];
+                            break;
+                    case 9: $weather = $forecast->forecasts[4];
+                            break;
+                }
+            } else {
+                switch ($hours) {
+                    case 3: $weather = $forecast->forecasts[1];
+                            break;
+                    case 6: $weather = $forecast->forecasts[2];
+                            break;
+                    case 9: $weather = $forecast->forecasts[3];
+                            break;
+                }
+            }
+            $array = array(
+                'vientoVel' => $weather->wind->speed,
+                'vientoDir' => $weather->wind->direction,
+                'humedad' => $weather->humidity,
+                'temperatura' => $weather->temperature,
+                'temperaturaMin' => $weather->temperature->min,
+                'temperaturaMax' => $weather->temperature->max,
+                'precipitaciones' => $weather->precipitation,
+                'presion' => $weather->pressure,
+                'fecha' => $weather->lastUpdate
+            );
         }
-        $array = array(
-            'vientoVel' => $weather->wind->speed,
-            'vientoDir' => $weather->wind->direction,
-            'humedad' => $weather->humidity,
-            'temperatura' => $weather->temperature,
-            'temperaturaMin' => $weather->temperature->min,
-            'temperaturaMax' => $weather->temperature->max,
-            'precipitaciones' => $weather->precipitation,
-            'presion' => $weather->pressure,
-            'fecha' => $weather->lastUpdate
-        );
-        //$array['notice_id'] = $notice->id;
 
         return $array;
     }
