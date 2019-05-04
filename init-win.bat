44b55197f5b4a6c39a2cdd4a92051b16540d04ff
Daniel Primitivo Cano <dpc32@alu.ua.es>
2019-05-04 16:56:08 +0200

    Configuración para el borrado en cascada

--- init-win.bat
+++ init-win.bat
@@ -4,6 +4,7 @@ start /WAIT .\composer-install-win.bat
 start /WAIT .\composer-require-win.bat
 php artisan vendor:publish --provider="Gmopx\LaravelOWM\LaravelOWMServiceProvider"
 php artisan key:generate
+copy SQLiteConnector.php.example ./vendor/laravel/framework/src/Illuminate/Database/Connectors/SQLiteConnector.php
 php artisan migrate:install
 php artisan migrate --seed
 PAUSE
--- init.sh
+++ init.sh
@@ -6,5 +6,7 @@ composer install
 composer require gmopx/laravel-owm
 php artisan vendor:publish --provider="Gmopx\LaravelOWM\LaravelOWMServiceProvider"
 php artisan key:generate
+cp SQLiteConnector.php.example ./vendor/laravel/framework/src/Illuminate/Database/Connectors/SQLiteConnector.php
 php artisan migrate:install
 php artisan migrate --seed
+
